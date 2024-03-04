86e777d536d6cd87cc873a3afd5aca1d306c5d39
kab0o0m <134916365+kab0o0m@users.noreply.github.com>
2024-03-05 01:43:16 +0800

    added showcase on homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,28 @@
 import Navbar from "./Navbar";
+import "./page.css";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <h1>Hello</h1>
+      <div className="showcase">
+        <div className="showcase-big">
+          <h1>Iphone Series</h1>
+          <a href="/shop">
+            <button>SHOP NOW</button>
+          </a>
+        </div>
+        <div className="showcase-small">
+          <div className="showcase-small-1">
+            <h2 className="speakers">Speakers</h2>
+            <h4 className="mini-home">mini home</h4>
+          </div>
+          <div className="showcase-small-2">
+            <h4 className="high-quality">high quality</h4>
+            <h2 className="sounds">Sounds</h2>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
