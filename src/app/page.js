12e5e1fdc103fdae2c16f91823aace30abbf84e1
kab0o0m <134916365+kab0o0m@users.noreply.github.com>
2024-03-05 13:24:02 +0800

    added showcase-2

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,8 @@ export default function Home() {
       <Navbar />
       <div className="showcase">
         <div className="showcase-big">
-          <h1>Iphone Series</h1>
+          <p>Iphone Series</p>
+
           <a href="/shop">
             <button>SHOP NOW</button>
           </a>
@@ -20,6 +21,42 @@ export default function Home() {
           <div className="showcase-small-2">
             <h4 className="high-quality">high quality</h4>
             <h2 className="sounds">Sounds</h2>
+          </div>
+        </div>
+      </div>
+
+      <div className="showcase-2">
+        <div className="showcase-2-1">
+          <div className="showcase-2-1-img">
+            <a href="/shop">
+              <button>SHOP NOW</button>
+            </a>
+          </div>
+          <p>Airpods</p>
+        </div>
+        <div className="showcase-2-2">
+          <p>Headphones</p>
+
+          <div className="showcase-2-2-img">
+            <a href="/shop">
+              <button>SHOP NOW</button>
+            </a>
+          </div>
+        </div>
+        <div className="showcase-2-3">
+          <div className="showcase-2-3-img">
+            <a href="/shop">
+              <button>SHOP NOW</button>
+            </a>
+          </div>
+          <p>Apple Watch</p>
+        </div>
+        <div className="showcase-2-4">
+          <p>Accessories</p>
+          <div className="showcase-2-4-img">
+            <a href="/shop">
+              <button>SHOP NOW</button>
+            </a>
           </div>
         </div>
       </div>
