183c106df7a4470515e64dc015099a2b5f823286
kab0o0m <134916365+kab0o0m@users.noreply.github.com>
2024-03-06 01:18:42 +0800

    added shop page

--- src/app/Account/register/page.js
+++ src/app/Account/register/page.js
@@ -28,27 +28,43 @@ export default function Register() {
     }));
   };
 
+  const isNumeric = (value) => {
+    return !isNaN(parseFloat(value)) && isFinite(value);
+  };
+
+  const isEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (formData.phoneNumber.length === 8 && isNumeric(formData.phoneNumber)) {
+      if (isEmail(formData.username)) {
+        const text = `{
+          "username": "${formData.username}",
+          "password": "${formData.password}",
+          "firstName": "${formData.firstName}",
+          "lastName": "${formData.lastName}",
+          "phoneNo": "${formData.phoneNumber}"
+        }`;
 
-    const text = `{
-        "username": "${formData.username}",
-        "password": "${formData.password}",
-        "firstName": "${formData.firstName}",
-        "lastName": "${formData.lastName}",
-        "phoneNo": "${formData.phoneNumber}"
-    }`;
+        const jsonData = JSON.parse(text);
+        console.log(jsonData);
 
-    const jsonData = JSON.parse(text);
-    console.log(jsonData);
-
-    try {
-      const register_url = "http://localhost:8080/api/auth/register";
-      const response = await axios.post(register_url, jsonData);
-      setIsCreate(true);
-      console.log(response);
-    } catch (error) {
-      console.error("Registration failed");
+        try {
+          const register_url = "http://localhost:8080/api/auth/register";
+          const response = await axios.post(register_url, jsonData);
+          setIsCreate(true);
+          console.log(response);
+        } catch (error) {
+          console.error("Registration failed");
+        }
+      } else {
+        alert("Provide a valid email!");
+      }
+    } else {
+      alert("Phone number must be 8 Digits!");
     }
   };
 
--- src/app/Shop/page.js
+++ src/app/Shop/page.js
@@ -1,20 +1,57 @@
+"use client";
+
 import Navbar from "../Navbar";
 import "./page.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Shop() {
+  const [allProducts, setAllProducts] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const products = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/api/product");
+      setAllProducts(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    products();
+  }, []);
+
+  console.log(allProducts);
+
   return (
     <>
       <Navbar />
-      {/* Header */}
-      <div className="shop">
-        <div className="shop-header">
-          <a href="/">Home</a>
-          <div>/</div>
-          <div className="shop-header-page">
-            <p>Shop</p>
+
+      {!isLoading && (
+        <div className="shop">
+          {/* Header */}
+          <div className="shop-header">
+            <a href="/">Home</a>
+            <div>/</div>
+            <div className="shop-header-page">
+              <p>Shop</p>
+            </div>
+          </div>
+          {/* Body */}
+          <div className="shop-body">
+            {allProducts.length !== 0 &&
+              Object.values(allProducts).map((product, index) => (
+                <div key={index} className="product">
+                  <p>{product.name}</p>
+                  <p>{product.description}</p>
+                  <p>{product.price}</p>
+                </div>
+              ))}
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
