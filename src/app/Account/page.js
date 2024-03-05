28f8f21f7c82a19dd2e2c70f9aaf0a402d14d049
kab0o0m <134916365+kab0o0m@users.noreply.github.com>
2024-03-05 17:45:50 +0800

    added register and login

--- src/app/Account/page.js
+++ src/app/Account/page.js
@@ -1,7 +1,44 @@
+"use client";
+
 import Navbar from "../Navbar";
 import "./page.css";
+import { useState } from "react";
+import axios from "axios";
 
 export default function Account() {
+  const initialFormData = {
+    email: "",
+    password: "",
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value, // Update the value for the corresponding input name
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const text = `{
+      "username": "${formData.email}",
+      "password": "${formData.password}"
+    }`;
+    const jsonData = JSON.parse(text);
+    console.log(jsonData);
+
+    try {
+      const login_url = "http://localhost:8080/api/auth/login";
+      await axios.post(login_url, jsonData);
+      console.log("Login");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <Navbar />
@@ -21,15 +58,26 @@ export default function Account() {
             <h1>Login</h1>
             <p>Please login using account detail below.</p>
           </div>
-          <form action="">
+          <form action="" onSubmit={handleSubmit}>
             <div className="email">
-              <input type="text" id="email" placeholder="Email" required />
+              <input
+                type="text"
+                id="email"
+                name="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleInputChange}
+                required
+              />
             </div>
             <div className="password">
               <input
                 type="text"
                 id="password"
+                name="password"
                 placeholder="Password"
+                value={formData.password}
+                onChange={handleInputChange}
                 required
               />
             </div>
--- src/app/Account/register/page.js
+++ src/app/Account/register/page.js
@@ -1,7 +1,52 @@
+"use client";
+
 import Navbar from "../../Navbar";
 import "./page.css";
+import { useState } from "react";
+import axios from "axios";
 
 export default function Register() {
+  const initialFormData = {
+    username: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value, // Update the value for the corresponding input name
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const text = `{
+        "username": "${formData.username}",
+        "password": "${formData.password}",
+        "firstName": "${formData.firstName}",
+        "lastName": "${formData.lastName}",
+        "phone": "${formData.phoneNumber}"
+    }`;
+
+    const jsonData = JSON.parse(text);
+    console.log(jsonData);
+    try {
+      const register_url = "http://localhost:8080/api/auth/register";
+      const response = await axios.post(register_url, jsonData);
+
+      console.log("User registered");
+    } catch (error) {
+      console.error("Registration failed");
+    }
+  };
+
   return (
     <>
       <Navbar />
@@ -21,31 +66,15 @@ export default function Register() {
             <h1>Create Account</h1>
             <p>Please register using account detail below.</p>
           </div>
-          <form action="">
-            <div className="first-name">
+          <form action="" onSubmit={handleSubmit}>
+            <div className="username">
               <input
                 type="text"
-                id="first-name"
-                placeholder="First Name"
-                required
-              />
-            </div>
-            <div className="last-name">
-              <input
-                type="text"
-                id="last-name"
-                placeholder="Last Name"
-                required
-              />
-            </div>
-            <div className="email">
-              <input type="text" id="email" placeholder="Email" required />
-            </div>
-            <div className="phone">
-              <input
-                type="text"
-                id="phone"
-                placeholder="Phone Number"
+                id="username"
+                name="username"
+                placeholder="Email"
+                value={formData.username}
+                onChange={handleInputChange}
                 required
               />
             </div>
@@ -53,10 +82,48 @@ export default function Register() {
               <input
                 type="text"
                 id="password"
+                name="password"
                 placeholder="Password"
+                value={formData.password}
+                onChange={handleInputChange}
                 required
               />
             </div>
+            <div className="first-name">
+              <input
+                type="text"
+                id="first-name"
+                name="firstName"
+                placeholder="First Name"
+                value={formData.firstName}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div className="last-name">
+              <input
+                type="text"
+                id="last-name"
+                name="lastName"
+                placeholder="Last Name"
+                value={formData.lastName}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+
+            <div className="phone">
+              <input
+                type="text"
+                id="phone"
+                name="phoneNumber"
+                placeholder="Phone Number"
+                value={formData.phoneNumber}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+
             <div className="enter">
               <button type="submit" className="register-button">
                 Create
