16f775fc5bcd089e9af38d00a5818f47026c7c87
jensonjenkins <dave.martheen@gmail.com>
2024-03-07 10:56:17 +0800

    change ui

--- src/app/page.js
+++ src/app/page.js
@@ -13,38 +13,49 @@ export default function Home() {
     <main>
       <Navbar />
       <CarouselComp />
-
+      <div className="mx-auto flex flex-col mb-32">
+        <h1 className="text-[50px] font-light mx-auto">Discover More</h1>
+        <h1
+          className={`${pfDisplay.className} text-[28px] font-light mx-auto -mt-3`}
+        >
+          <Em>
+            from <Strong className="font-semibold">Volt</Strong>
+          </Em>
+        </h1>
+      </div>
       <div className="showcase-2">
         <div className="showcase-2-1">
           <div className="showcase-2-1-img">
             <a href="/shop">
-              <button className="shopnow-button">SHOP NOW</button>
+              <button className="shopnow-button">Shop Airpods</button>
             </a>
           </div>
-          <p>Airpods</p>
+          <p className="text-[30px]">Airpods</p>
         </div>
         <div className="showcase-2-2">
-          <p>Headphones</p>
+          <p className="text-[30px]">Headphones</p>
 
           <div className="showcase-2-2-img">
             <a href="/shop">
-              <button className="shopnow-button">SHOP NOW</button>
+              <button className="shopnow-button font-semibold">
+                Shop Headphones
+              </button>
             </a>
           </div>
         </div>
         <div className="showcase-2-3">
           <div className="showcase-2-3-img">
             <a href="/shop">
-              <button className="shopnow-button">SHOP NOW</button>
+              <button className="shopnow-button">Shop Watch</button>
             </a>
           </div>
-          <p>Apple Watch</p>
+          <p className="text-[30px]">Apple Watch</p>
         </div>
         <div className="showcase-2-4">
-          <p>Accessories</p>
+          <p className="text-[30px]">Accessories</p>
           <div className="showcase-2-4-img">
             <a href="/shop">
-              <button className="shopnow-button">SHOP NOW</button>
+              <button className="shopnow-button">Shop Accessories</button>
             </a>
           </div>
         </div>
