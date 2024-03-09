a4cf1b90e472a05dc706d02ea83867a9359be482
jensonjenkins <dave.martheen@gmail.com>
2024-03-09 14:01:36 +0800

    feature: remove from cart

--- src/app/components/ShoppingCartSheet.js
+++ src/app/components/ShoppingCartSheet.js
@@ -6,6 +6,8 @@ import { MdShoppingCart } from "react-icons/md";
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { RxCross2 } from "react-icons/rx";
+import { useToast } from "@/components/ui/use-toast";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import {
   Sheet,
@@ -19,6 +21,7 @@ import {
 } from "@/components/ui/sheet";
 
 const ShoppingCartSheet = () => {
+  const { toast } = useToast();
   const [myCartItems, setMyCartItems] = useState([]);
   const [total, setTotal] = useState(null);
   const getMyCartItems = async () => {
@@ -39,6 +42,37 @@ const ShoppingCartSheet = () => {
   useEffect(() => {
     getMyCartItems();
   });
+  const removeFromCart = async (targetProductId) => {
+    try {
+      const sessionData = JSON.parse(localStorage.getItem("session"));
+      if (sessionData && sessionData.id) {
+        const response = await axios.delete(
+          "http://localhost:8080/api/cart_item/remove_item",
+          { data: { sessionId: sessionData.id, productId: targetProductId } }
+        );
+        console.log("targetproductId: ", targetProductId);
+        console.log(response);
+      }
+
+      const sessionResponse = await axios.post(
+        "http://localhost:8080/api/session/total",
+        {
+          sessionId: sessionData.id,
+        }
+      );
+      localStorage.setItem("session", JSON.stringify(sessionResponse.data));
+      toast({
+        title: "Item removed from cart!",
+      });
+    } catch (error) {
+      console.error("Error fetching deleting cart item:", error);
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: "There was a problem with your request.",
+      });
+    }
+  };
 
   return (
     <Sheet>
@@ -56,13 +90,28 @@ const ShoppingCartSheet = () => {
         <ScrollArea className="h-[300px]">
           {myCartItems.map((item, index) => {
             return (
-              <CartItem
-                key={index}
-                price={item.price}
-                imageLink={item.imageLink}
-                quantity={item.quantity}
-                name={item.name}
-              />
+              <>
+                <div className="flex flex-row border-t justify-between">
+                  <CartItem
+                    key={index}
+                    price={item.price}
+                    imageLink={item.imageLink}
+                    quantity={item.quantity}
+                    name={item.name}
+                  />
+                  <div>
+                    <button
+                      className="p-2"
+                      onClick={() => {
+                        removeFromCart(item.productId);
+                        getMyCartItems();
+                      }}
+                    >
+                      <RxCross2 className="" size={24} color="gray" />
+                    </button>
+                  </div>
+                </div>
+              </>
             );
           })}
         </ScrollArea>
